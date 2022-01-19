4e1b110a72519e6ba48f98338b36306c25b419aa
mabus <m.buscaroli@outlook.com>
2022-01-19 14:58:33 +0000

    Initial Commit

---
+++ swiftCombine/swiftCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftCombineApp.swift
+//  swiftCombine
+//
+//  Created by Matteo on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
